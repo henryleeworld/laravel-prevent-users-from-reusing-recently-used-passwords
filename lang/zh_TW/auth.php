e2adfe8269c3d4cca1dfedbbf798b6af4a97142d
Henry Lee <henryleeworld@gmail.com>
2024-02-04 01:43:06 +0800

    feat:更新最新資料

--- lang/zh_TW/auth.php
+++ lang/zh_TW/auth.php
@@ -1,19 +1,10 @@
 <?php
 
-/*
-|--------------------------------------------------------------------------
-| Authentication Language Lines
-|--------------------------------------------------------------------------
-|
-| The following language lines are used during authentication for various
-| messages that we need to display to the user. You are free to modify
-| these language lines according to your application's requirements.
-|
-*/
+declare(strict_types=1);
 
 return [
     'failed'   => '使用者名稱或密碼錯誤。',
-    'password' => '密碼錯誤。',
+    'password' => '密碼錯誤',
     'password_history' => '密碼先前已使用過，無法重複使用。',
     'throttle' => '嘗試登入太多次，請在 :seconds 秒後再試。',
 ];
